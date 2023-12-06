5be571dd950dd561deda3b8976381a1ce1be983d
FilipZmijewski <ltmijews@student.agh.edu.pl>
2023-12-06 23:13:29 +0100

    Created first components to prepare Login forms

---
+++ frontend/src/Login.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+
+export function Login() {
+  const count = useSelector();
+  const dispatch = useDispatch();
+
+  return <div></div>;
+}
