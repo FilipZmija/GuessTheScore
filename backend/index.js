1bab959c620549868aa43401cb86e55d584b9471
FilipZmijewski <ltmijews@student.agh.edu.pl>
2023-12-11 23:31:58 +0100

    Added modelss for score and user guesses and created connection i DB between them

--- backend/index.js
+++ backend/index.js
@@ -3,10 +3,16 @@ const app = express();
 const cors = require("cors");
 const db = require("./models");
 const user = require("./routes/login");
+const event = require("./routes/events");
+const guess = require("./routes/guess");
+
 require("dotenv").config();
 const cookieParser = require("cookie-parser");
 
 app.use("/user", user);
+app.use("/event", event);
+app.use("/guess", guess);
+
 app.use(express.json({ limit: "10mb" }));
 app.use(cors());
 app.use(cookieParser());
