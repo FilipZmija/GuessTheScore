ffc8304b8fb0b292a6f03d465919e556c46a580b
FilipZmijewski <ltmijews@student.agh.edu.pl>
2023-12-13 23:46:21 +0100

    Function to get any date

---
+++ backend/date.js
@@ -0,0 +1,13 @@
+const getDate = (index) => {
+  const today = new Date();
+  today.setDate(today.getDate() + index);
+
+  const year = today.getFullYear();
+  const month = String(today.getMonth() + 1).padStart(2, "0"); // Month is zero-based
+  const day = String(today.getDate()).padStart(2, "0");
+
+  const formattedDate = `${year}-${month}-${day}`;
+  console.log(formattedDate);
+  return formattedDate;
+};
+module.exports = { getDate };
