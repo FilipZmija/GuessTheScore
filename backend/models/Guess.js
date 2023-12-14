072d6603fc748c83e27533375ed876485e59aadc
FilipZmijewski <ltmijews@student.agh.edu.pl>
2023-12-14 16:51:01 +0100

    Minor changes

--- backend/models/Guess.js
+++ backend/models/Guess.js
@@ -1,10 +1,21 @@
 module.exports = (sequelize, DataTypes) => {
-  const Guess = sequelize.define("Guess", {
-    score: {
-      type: DataTypes.STRING,
-      allowNull: false,
+  const Guess = sequelize.define(
+    "Guess",
+    {
+      score: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
     },
-  });
+    {
+      indexes: [
+        {
+          unique: true,
+          fields: ["UserId", "EventId"],
+        },
+      ],
+    }
+  );
   Guess.associate = (models) => {
     Guess.belongsTo(models.Users);
     Guess.belongsTo(models.Event);
