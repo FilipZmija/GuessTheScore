b24202c1a9526268c95f5b7be7f1b533403b0064
FilipZmijewski <ltmijews@student.agh.edu.pl>
2023-12-18 23:39:19 +0100

    created login form added endpoints for login and created css file

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,20 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import store from "./redux/store";
 import { Provider } from "react-redux";
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "./theme";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
     </Provider>
   </React.StrictMode>
 );
