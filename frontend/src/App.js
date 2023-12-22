1e7850466ac31927f2ec608174a44e2460d44922
FilipZmijewski <ltmijews@student.agh.edu.pl>
2023-12-22 21:41:01 +0100

    Minor changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,16 @@ import React from "react";
 import { useSelector } from "react-redux";
 import Auth from "./Auth/Auth";
 import GuessApp from "./Main/GuessApp";
+import NavBar from "./NavBar";
 const App = () => {
   const token = useSelector((state) => state.auth.token);
   console.log(token);
-  return <>{localStorage.getItem("token") ? <GuessApp /> : <Auth />}</>;
+  return (
+    <>
+      <NavBar />
+      {token ? <GuessApp /> : <Auth />}
+    </>
+  );
 };
 
 export default App;
