bed2d831d5cb2201fde5fb985becb0046d40c26b
Zmija <94125339+FilipZmija@users.noreply.github.com>
2023-12-22 21:56:51 +0100

    Revert "Started craeting list of games fo each day"

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,11 @@
-import React from "react";
-import { useSelector } from "react-redux";
-import Auth from "./Auth/Auth";
-import GuessApp from "./Main/GuessApp";
-import NavBar from "./NavBar";
-const App = () => {
-  const token = useSelector((state) => state.auth.token);
-  console.log(token);
+import "./App.css";
+import { Counter } from "./Login";
+function App() {
   return (
-    <>
-      <NavBar />
-      {token ? <GuessApp /> : <Auth />}
-    </>
+    <div className="App">
+      <Counter />
+    </div>
   );
-};
+}
 
 export default App;
