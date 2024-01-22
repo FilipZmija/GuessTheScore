ca7bf38579748cd1b6894e0f7cdac237ba3ff5a3
FilipZmijewski <ltmijews@student.agh.edu.pl>
2024-01-22 23:48:57 +0100

    Changes to redux store

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,7 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authSlice from "./authSlice";
 import eventsSlice from "./eventsSlice";
-
+import scoreboardSlice from "./scoreboardSlice";
 export default configureStore({
-  reducer: { auth: authSlice, events: eventsSlice },
+  reducer: {
+    auth: authSlice,
+    events: eventsSlice,
+    scoreboard: scoreboardSlice,
+  },
 });
