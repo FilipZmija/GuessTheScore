07284174fc720618a32060ede62efe238d7ea113
FilipZmijewski <ltmijews@student.agh.edu.pl>
2024-01-24 16:28:44 +0100

    Add popularguesses suggestions when tehre is no guesses:

--- frontend/src/Main/PopularGuesses.jsx
+++ frontend/src/Main/PopularGuesses.jsx
@@ -1,13 +1,46 @@
 import React, { useEffect } from "react";
 import Button from "@mui/material/Button";
 import { Box, Typography } from "@mui/material";
-import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { guessScore, setIsClicked, setSelectedGame } from "../redux/guessSlice";
+import {
+  guessScore,
+  setIsClicked,
+  setPopularGuesses,
+} from "../redux/guessSlice";
 export default function PopularGuesses() {
   const { isClicked, popularGuesses, selectedGame } = useSelector(
     (state) => state.guess
   );
+  useEffect(() => {
+    if (!popularGuesses?.length) {
+      const generateGuesses = () => {
+        const guesses = [
+          "1:0",
+          "1:1",
+          "2:0",
+          "2:1",
+          "2:2",
+          "3:0",
+          "3:1",
+          "3:2",
+          "3:3",
+          "4:0",
+          "4:1",
+          "4:2",
+          "4:3",
+          "4:4",
+        ];
+        const popular = [];
+        for (let i = 0; i < 3; i++) {
+          const index = Math.floor(Math.random() * guesses.length);
+          popular.push({ number: 0, score: guesses[index] });
+          guesses.splice(index, 1);
+        }
+        return popular;
+      };
+      dispatch(setPopularGuesses(generateGuesses()));
+    }
+  }, []);
   const dispatch = useDispatch();
   const handleToggle = (index, score) => {
     dispatch(setIsClicked(index));
